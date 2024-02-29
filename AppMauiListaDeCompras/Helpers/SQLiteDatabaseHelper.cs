9b72c40059979c599b1523920cdb3f4381ebd46b
Usuario <Usuario@CPD03-M09>
2024-02-29 15:00:44 -0300

    Avanços SQLite

---
+++ AppMauiListaDeCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using AppMauiListaDeCompras.Models;
+
+namespace AppMauiListaDeCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update (Produto p)
+        {
+            string sql = "Update Prudote SET Descricao=?, " +
+                         "Preco=? Quantidade=? WHERE id=?";
+
+            return _conn.QueryAsync<Produto>(sql,
+                p.Descricao, p.Preco, p.Quantidade, p.Id);
+        }
+
+        public Task<int> Delelte(int id)
+        { 
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+    }
+}
---
+++ AppMauiListaDeCompras/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+namespace AppMauiListaDeCompras.Models
+{
+    public class Produto
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
+        public double Total { get; set; }
+    }
+}
